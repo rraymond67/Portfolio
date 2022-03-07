2d048419c4872afc2201c2e9f281b7c2738f554e
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-03-07 01:32:55 -0500

    begin

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
-export default function nav() {
+export default function Nav() {
   return (
-    <nav>
-      <h1> Rod R. Raymond</h1>
-      <div>
-        
-      </div>
-    </nav>
+    <div class="py-5 flex flex-col items-center" >
+      <ul class="flex row space-x-10" >
+          <li>
+            <a href="#about" class="font-serif underline">About Me</a>
+            </li>
+          <li>
+            <a href="#projects" class="font-serif underline">Projects</a>
+          </li>
+          <li>
+            <a href="#contacts" class="font-serif underline">Contacts</a>
+          </li>
+        </ul>
+    </div>
   )
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,49 @@
 import React from 'react'
-import Nav from '../components/Nav'
+import Nav from '../components/Nav.jsx'
+
 export default function Home() {
   return (
-    <div>
-      <Nav />
-      <div>
-        <img src='head.jpeg' alt='head shot of Rod Raymond'/>
+    <div class="bg-stone-200">
+      <Nav/>
+      <div class="mt-10 flex flex-col items-center pb-20">
+        <img class="w-50 h-60 rounded-2xl shadow-lg"
+        src='head.jpeg' alt='head shot of Rod Raymond'/>
+        <hr class="my-6 border-black-900 dark:border-black-700 lg:my-8" />
+        <div class="flex h-200 bg-slate-100 items-center">
+        <p class="font-mono ml-20 mr-20 mt-10 mb-10 text-center "> "  I am a Multilingual Project Manager turned Full Stack Developer with a curiosity to learn
+          and a drive for new experiences. My international background and my love for languages has
+          provided me with the skills to communicate effectively in diverse environments and solve problems efficiently  ".</p>
+        </div>
       </div>
     
+      <div id ="about" class="flex justify-center">
+        <h1 class="text-3xl font-serif">Skills</h1>
+        <div class="mr-20 mb-20 grid grid-cols-4 gap-4 mt-20 space-x-20 ">
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt="javascript icon" />
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react icon" />
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="express icon"/>
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original-wordmark.svg" alt="mongoDB icon"/>
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" alt="postgreSQL icon"/>
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" alt="github icon"/>
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ruby/ruby-plain-wordmark.svg" alt="ruby icon"/>
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="css icon"/>
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="html icon"/>
+            <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/rails/rails-plain-wordmark.svg" alt="rails icon"/>
+            <img class="w-20 h-20" src="https://raw.githubusercontent.com/devicons/devicon/2ae2a900d2f041da66e950e4d48052658d850630/icons/tailwindcss/tailwindcss-original-wordmark.svg" alt="rails icon"/>
+         </div>
+       </div>
+
+       <div id="projects" class="flex justify-center">
+          <h1 class="text-3xl font-serif">Projects</h1>
+        <div>
+          
+        </div>
+
+          </div>
+
+          <div id="contacts" class="flex justify-center">
+          <h1 class="text-3xl font-serif">Contact</h1>
+          </div>
     </div>
   )
 }
