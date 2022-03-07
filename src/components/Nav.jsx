7ff346f48465cf21a29e379f97d9ce85106d347d
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-03-06 21:27:36 -0500

    begin

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function nav() {
+  return (
+    <nav>
+      <h1> Rod R. Raymond</h1>
+      <div>
+        
+      </div>
+    </nav>
+  )
+}
---
+++ src/screens/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../components/Nav'
+export default function Home() {
+  return (
+    <div>
+      <Nav />
+      <div>
+        <img src='head.jpeg' alt='head shot of Rod Raymond'/>
+      </div>
+    
+    </div>
+  )
+}
