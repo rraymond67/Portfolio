eed23c0b7906637c0b07e122a903146ed263ba4d
rraymond67 <93887904+rraymond67@users.noreply.github.com>
2022-03-08 22:26:41 -0500

    begin

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <div class="py-5 flex flex-col items-center" >
+    <div class="py-5 border-b-2 border-red-500 flex flex-col items-center" >
       <ul class="flex row space-x-10" >
           <li>
-            <a href="#about" class="font-serif underline">About Me</a>
+            <a href="#about" class="hover:text-white underline decoration-white font-serif underline">About Me</a>
             </li>
           <li>
-            <a href="#projects" class="font-serif underline">Projects</a>
+            <a href="#projects" class="hover:text-white underline decoration-white font-serif underline">Projects</a>
           </li>
           <li>
-            <a href="#contacts" class="font-serif underline">Contacts</a>
+            <a href="#contacts" class="hover:text-white underline decoration-white font-serif underline">Contacts</a>
           </li>
         </ul>
     </div>
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -13,17 +13,20 @@ export default function Home() {
           <p class="font-mono ml-20 mr-20 mt-10 mb-10 text-center "> "  I am a Multilingual     Project Manager turned Full Stack Developer with a curiosity to learn
           and a drive for new experiences. My international background and my love for languages has
           provided me with the skills to communicate effectively in diverse environments and solve problems efficiently ".</p>
-          <a class="mb-5 font-bold italic hover:not-italic underline decoration-red-500" href="https://docs.google.com/document/d/15U0gGEivgyqzkuVeNr9956PAqxViFcNGjgBwFUqVjAA/edit?usp=sharing" target="_blank" rel="noreferrer">
+          <a class="flex flex-row mb-5 hover:text-white pl-4 pr-4 bg-yellow-300 ps-4 py-2 rounded-lg font-bold italic hover:not-italic underline decoration-white" href="https://docs.google.com/document/d/15U0gGEivgyqzkuVeNr9956PAqxViFcNGjgBwFUqVjAA/edit?usp=sharing" target="_blank" rel="noreferrer">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
+          </svg>
             Resume
           </a>
         </div>
 
       </div>
-      <hr class="mb-20 mr-40 ml-40 dark:border-black-900" />
+      <hr className="mb-20 mr-40 ml-40 dark:border-black-900" />
     
       <div id ="about" class="mb-10 flex flex flex-col items-center">
-        <h1 class="text-3xl font-serif">Skills</h1>
-        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center drop-shadow-2xl gap-5 mt-10 space-x-20 ">
+        <h1 class="text-3xl underline font-serif">Skills</h1>
+        <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-items-center drop-shadow-2xl gap-20 mt-10">
             <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg" alt="javascript icon" />
             <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" alt="react icon" />
             <img class="w-20 h-20" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original-wordmark.svg" alt="express icon"/>
@@ -41,8 +44,8 @@ export default function Home() {
        <hr class="mb-20 mr-40 ml-40 dark:border-gray-800" />
 
        <div id="projects" class="mb-10 flex flex flex-col items-center">
-          <h1 class="text-3xl font-serif">Projects</h1>
-          <div class="mt-10 mr-40 ml-40 flex grid grid-cols-1 justify-items-center">
+          <h1 class="text-3xl underline font-serif">Projects</h1>
+          <div class="mt-10 sm:mr-10 sm:ml-10 md:mr-40 md:ml-40 flex grid grid-cols-1 justify-items-center">
             <div id="accordion-collapse" data-accordion="collapse">
               <h2 id="accordion-collapse-heading-1">
               <button type="button" class="flex justify-between items-center p-5 w-full font-medium text-left text-gray-900 bg-gray-100 rounded-t-xl border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-white dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
@@ -53,7 +56,7 @@ export default function Home() {
             <div id="accordion-collapse-body-1" aria-labelledby="accordion-collapse-heading-1">
                <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
                  <p class="mb-2 text-gray-500 dark:text-gray-400">Life After General Assembly is a website for finding full-stuck materials. This web application is a great ressource for bootcamp graduates and developers alikes who are looking to futher their knowledge. Users will be able to navigate thru a list of courses and see comments about specific courses from the existing users who have signed up. Evey course will be categorized accordingly. In order to leave comments or add a new course, one will need log in/sign up.</p>
-                <p class="text-gray-500 dark:text-gray-400">Check out the website: <a href="https://silly-kowalevski-1591fc.netlify.app/" class="text-blue-600 dark:text-blue-500 hover:underline">L.A.G.A</a></p>
+                <p class="text-gray-500 dark:text-gray-400">Check out the website: <a href="https://silly-kowalevski-1591fc.netlify.app/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline">L.A.G.A</a></p>
              </div>
            </div>
            
@@ -66,7 +69,7 @@ export default function Home() {
              <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
                 <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700">
                    <p class="mb-2 text-gray-500 dark:text-gray-400">WePay is an online commerce application. Its a place where you post items for sale and other users will be able to place bids. In order to place bids or post an item you must sign up. The search bar will help users filter through the long list of items for sale. Once they find the items they are interest in, they click on it and it will navigate them to the details page where they eill see the item's description and the list of bidders. Users who are not sign in will still be able to navigate the website but they wont be able to place bids or post items for sale.</p>
-                    <p class="text-gray-500 dark:text-gray-400">Check out the webiste: <a href="https://wepayrod.netlify.app" class="text-blue-600 dark:text-blue-500 hover:underline">WePay</a></p>
+                    <p class="text-gray-500 dark:text-gray-400">Check out the webiste: <a href="https://wepayrod.netlify.app" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline">WePay</a></p>
                 </div>
               </div>
 
@@ -79,7 +82,7 @@ export default function Home() {
            <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
             <div class="p-5 border border-t-0 border-gray-200 dark:border-gray-700">
                <p class="mb-2 text-gray-500 dark:text-gray-400">A react application where you can apply to the Hogwarts School of Witchcraft and Wizardry and choose which house to inlist in. You will be given a student application form to submit to the school. Once you fill out the information required on the form, you submit it for request approval. If you dont feel confident about your application you can delete it (or edit it). You can also check a list of Alumni(You might get accepted or rejected)..</p>
-               <p class="text-gray-500 dark:text-gray-400">Check out the webiste: <a href="https://school-for-wizards.netlify.app/" class="text-blue-600 dark:text-blue-500 hover:underline">Hogwarts School of Witchcraft and Wizardry</a></p>
+               <p class="text-gray-500 dark:text-gray-400">Check out the webiste: <a href="https://school-for-wizards.netlify.app/" target="_blank" rel="noreferrer" class="text-blue-600 dark:text-blue-500 hover:underline" >Hogwarts School of Witchcraft and Wizardry</a></p>
     </div>
   </div>
 </div>
@@ -93,7 +96,7 @@ export default function Home() {
           <hr class="mb-20 mr-40 ml-40 dark:border-gray-700" />
 
           <div id="contacts" class="mb-10 flex flex-col items-center">
-          <h1 class="mb-10 text-3xl font-serif">Contact</h1>
+          <h1 class="mb-10 text-3xl underline font-serif">Contacts</h1>
           <div class="flex mb-20 grid grid-cols-1 justify-items-center">
             <h2>Email: rod.raymond67@gmail.com</h2>
             <h2>LinkedIn: <a href="https://www.linkedin.com/in/rod-raymond-281a0284/" target="_blank" rel="noreferrer">Rod Raymond</a>
